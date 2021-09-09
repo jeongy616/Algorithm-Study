05efdf674a9b45a796a895d8f3a355b221bc4818
jeongyeonkim <jeongy616@naver.com>
2021-09-09 22:47:52 +0900

    Algorithm-Study/programmers/MenuRenewal.py: update MenuRenewal python file

--- programmers/MenuRenewal.py
+++ programmers/MenuRenewal.py
@@ -48,3 +48,21 @@ def solution(orders, course):
 
 # print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"],[2,3,4]))
 print(solution(["XYZ", "XWY", "WXA"],[2,3,4]))
+
+
+import collections
+import itertools
+
+# 고인물 코드 공부해 보자...
+def solution2(orders, course):
+    result = []
+
+    for course_size in course:
+        order_combinations = []
+        for order in orders:
+            order_combinations += itertools.combinations(sorted(order), course_size)
+
+        most_ordered = collections.Counter(order_combinations).most_common()
+        result += [ k for k, v in most_ordered if v > 1 and v == most_ordered[0][1] ]
+
+    return [ ''.join(v) for v in sorted(result) ]
